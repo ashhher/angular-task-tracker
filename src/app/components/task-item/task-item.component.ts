b62af3ffa7386eb499fb3461fd7edc4a73f6c7cb
Han Xiao <asher.han.xiao@gmail.com>
2024-04-26 15:17:36 +1000

    feat: update task reminder

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -2,20 +2,26 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
   @Input() task: Task | undefined;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   onDelete(task: Task | undefined) {
     this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task | undefined) {
+    this.onToggleReminder.emit(task);
   }
 }
